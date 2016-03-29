38628ef06097253f13bccb7062c3768266dede40
Steve Liddle <liddle@byu.edu>
2016-03-29 11:08:27 -0600

    Refactored in preparation for letting students help on this project.

--- script/utilities.js
+++ script/utilities.js
@@ -17,3 +17,26 @@ if (window.jQuery !== undefined) {
         });
     };
 }
+
+var globalToggleCollapse = function (event, collapseId, iconId) {
+    var volumeId = event.target.dataArgs;
+    var collapse;
+    var icon;
+    var rotate;
+
+    if (volumeId === undefined) {
+        volumeId = event.target.parentElement.dataArgs;
+    }
+
+    collapse = document.querySelector('#' + collapseId + volumeId);
+    icon = document.querySelector('#' + iconId + volumeId);
+
+    rotate = collapse.opened ? 180 : 0;
+    collapse.toggle();
+
+    if (window.$ !== undefined && $.fn.animateRotate !== undefined) {
+        $(icon).animateRotate(rotate, 180 - rotate, 200);
+    } else {
+        icon.style.transform = "rotate(" + rotate + "deg)";
+    }
+}
